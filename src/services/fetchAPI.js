ae68d2c05e9fca5bb87e3c1c98f8645b5fbaf9f6
iNK <>
2023-10-16 01:19:22 +0530

    Initial Commit

--- src/services/fetchAPI.js
+++
@@ -1,6 +0,0 @@
-export const apiCall = async () =>{
-    const api = `https://rickandmortyapi.com/api/character/?count=30`
-    const data = await fetch(api);
-    const realData = await data.json();
-    return (realData.results);
-}
--- src/store/AuthContextV2.jsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const AuthContextV2 = React.createContext();
